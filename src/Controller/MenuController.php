b7b610d160cc2c81a6e46a72775222f0ab707530
Edgar <edgarpinheiro3@gmail.com>
2023-07-18 09:47:14 -0300

    Show menu card
    Autenticação no Menu
    Slide no /

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -5,30 +5,24 @@ namespace App\Controller;
 use App\Entity\Prato;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class MenuController extends AbstractController
 {
 
     public function __construct(private ManagerRegistry $doctrine) {
 
     }
 
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    #[Route('/menu', name: 'menu')]
+    public function menu()
     {
 
         $em = $this->doctrine->getManager();
         $prato = $em->getRepository(Prato::class)->findAll();
 
-        $random = array_rand($prato, 2);
-        //dump($prato[$random[0]]);
-        //dump($prato[$random[1]]);
-
-        return $this->render('home/index.html.twig', [
-            'prato1' => $prato[$random[0]],
-            'prato2' => $prato[$random[1]],
+        return $this->render('menu/index.html.twig', [
+            'prato' => $prato,
         ]);
     }
 }
